1399346df47c0bd21b5b0ce8782424c7ea32ca16
Alex Agreda <agredalex1997@gmail.com>
2018-12-26 13:16:51 -0600

    first commit from thinkpad

--- expressjs/index.js
+++ expressjs/index.js
@@ -3,7 +3,19 @@ const app = express();
 const port = 3000;
 
 app.get("/", (req, res) => {
-  res.send(`Hello, world!`);
+  res.send(`${req}`);
+});
+
+app.post("/", (req, res) => {
+  res.send("Got a POST request");
+});
+
+app.put("/user", function(req, res) {
+  res.send("Got a PUT request at /user");
+});
+
+app.delete("/user", function(req, res) {
+  res.send("Got a DELETE request at /user");
 });
 
 app.listen(port, () => {
