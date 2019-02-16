92ea3b5c19189cb491687dfe490eb888fb937046
Alex Ágreda <agredalex1997@gmail.com>
2019-02-15 22:22:45 -0600

    cleaning

--- node/scripts.js
+++
@@ -1,7 +0,0 @@
-var fs = require('fs');
-var file = fs.readFile('file.txt', (err, data) => {
-	console.log(data.toString());
-});
-
-console.log('Outside');
-//console.log(file.toString());
--- vue/js/main.js
+++
@@ -1,6 +0,0 @@
-let growler = new Vue({
-  el: '#growler',
-  data: {
-    appName: 'Growler',
-  }
-});
